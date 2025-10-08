c916194793009c92787337bb0a69aefe32be1089
ajb040 <ajb040@bucknell.edu>
2025-10-07 23:14:30 -0400

    Created an apache log generator

---
+++ apache-log-generator.py
@@ -0,0 +1,21 @@
+from faker import Faker
+import random
+from datetime import datetime, timedelta
+
+fake = Faker()
+methods = ["GET", "POST", "PUT", "DELETE"]
+endpoints = ["/", "/index.html", "/login", "/dashboard", "/api/data", "/contact"]
+status_codes = [200, 404, 500, 403, 301]
+
+with open("access.log","w") as f:
+    for i in range(1000):
+        ip = fake.ipv4()
+        time = (datetime.now() - timedelta(seconds=random.randint(0,100000))).strftime("%d/%b/%Y:%H:%M:%S -0400")
+        method = random.choice(methods)
+        endpoint = random.choice(endpoints)
+        status = random.choice(status_codes)
+        size = random.randint(200,5000)
+        line = f'{ip} - - [{time}] "{method} {endpoint} HTTP/1.1" {status} {size}\n'
+        f.write(line)
+
+print("access.log generated")
